be9629e7f044087b7da051512a99feab590dd42b
KritishAlli <kritish@outlook.com>
2026-02-14 17:18:36 -0800

    Initial Commit

---
+++ BirdTreeIdentifier/BirdTreeIdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdTreeIdentifierApp.swift
+//  BirdTreeIdentifier
+//
+//  Created by Kritish Alli on 2/14/26.
+//
+
+import SwiftUI
+
+@main
+struct BirdTreeIdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirdTreeIdentifierTests/BirdTreeIdentifierTests.swift
@@ -0,0 +1,17 @@
+//
+//  BirdTreeIdentifierTests.swift
+//  BirdTreeIdentifierTests
+//
+//  Created by Kritish Alli on 2/14/26.
+//
+
+import Testing
+@testable import BirdTreeIdentifier
+
+struct BirdTreeIdentifierTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
